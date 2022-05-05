45fd2b27d7f788b3bbe5caff5b6fbeedcc380224
mlocc <anthonymlocc@gmail.com>
2022-05-05 01:47:39 -0500

    login 80%

--- index.php
+++ index.php
@@ -6,13 +6,46 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tienda Marquez</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type = "text/css" href="../css/style.css">
-    <link rel="stylesheet" type = "text/css" href="../css/css/all.min.css">
+    <link rel="stylesheet" type = "text/css" href="css/style.css">
+    <link rel="stylesheet" type = "text/css" href="css/css/all.min.css">
 
 </head>
 <body>
-    <p>HOLA MUNDO</p>
-    <h1>Anthony Laptop  soy mlocc en el commit</h1>
-    <h1>Anthony Comput  soy stormlocc en el commit</h1>
+    <img class="wave" src="imagenes/wave.png" alt="">
+    <div class="contenedor">
+        <div class="img">
+            <img src="imagenes/usuario.png" alt="">
+        </div>
+        <div class="contenido-login">
+            <form action="">
+                <img src="imagenes/usuario1.png" alt="">
+                <h2>Tienda Marquez</h2>
+                <div class="input-login dni">
+                    <div class="i">
+                        <i class="fas fa-user "> </i>
+                    </div>
+                    <div class="div">
+                        <h5>Usuario o DNI</h5>
+                        <input type="text" name="user" class="input" >
+                    </div>
+                </div>
+                <div class="input-login pass">
+                    <div class="i">
+                        <i class="fas fa-lock "> </i>
+                    </div>
+                    <div class="div">
+                        <h5>Contraseña</h5>
+                        <input type="password" name="pass" class="input" >
+                    </div>
+                </div>
+                <input type="submit" class="btn" value="Iniciar Sesion">
+            </form>
+        </div>
+
+    </div>
+
 </body>
+
+<script src="js/login.js"></script>
+
 </html>
