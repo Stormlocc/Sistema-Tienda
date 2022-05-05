1590762d764e685f42f265e545c0a47afba3bce8
mlocc <anthonymlocc@gmail.com>
2022-05-04 22:55:54 -0500

    agregando fuente, imagenes y css

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tienda Marquez</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type = "text/css" href="../css/style.css">
+    <link rel="stylesheet" type = "text/css" href="../css/css/all.min.css">
+
 </head>
 <body>
     <p>HOLA MUNDO</p>
